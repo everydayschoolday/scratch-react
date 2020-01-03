1b1bb6cd1de72ea829c753071861a186b0eecdd4
Arti Mathanda <arti.mathanda@simplybusiness.co.uk>
2020-01-02 23:37:42 -0800

    babel config

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+// babel.config.js
+const presets = [
+  [
+    "@babel/preset-env",
+    {
+      // Pass a config object to the preset
+      debug: true // Output the targets/plugins used when compiling
+    }
+  ]
+];
+
+const plugins = [];
+
+// Export a config object.
+module.exports = { presets, plugins };
