71d5b209b4abe37329ce4d6ce7717d14c7f7726c
Arti Mathanda <arti.mathanda@simplybusiness.co.uk>
2020-01-03 00:15:52 -0800

    adds sum js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import "core-js/stable"; // Loads all language features
+import sum from "./sum";
+
+console.log(sum(2, 4));
 
 const greet = name => console.log(`Hello ${name}`);
 greet("Jon Snow");
